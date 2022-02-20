f2fd8ebbedd8d880239d9bebd8141e8decc8dfd4
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-02-20 17:17:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,30 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <Weather city="Paris" />
+    <div class="container">
+      <div class="weather-app-wrapper">
+        <div class="weather-app">
+          <div class="card-body">
+            <div class="row">
+              <Weather city="Paris" />
+              <br></br>
+              <footer>
+                <a
+                  href="https://github.com/elic4vet/weather-app"
+                  target="blank"
+                >
+                  Open-source code
+                </a>{" "}
+                by{" "}
+                <a href="https://www.linkedin.com/in/elisavet-erkekoglou-80514b107/">
+                  {" "}
+                  Elisavet Erkekoglou 👩‍💻{" "}
+                </a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
